6161ba0d3e4cc1ad1d61a2b6d1a9e99c2a379218
atiwat02 <atiwat.ka.61@ubu.ac.th>
2020-03-10 01:46:43 +0700

    update

--- ProJectDatasci0812/urls.py
+++ ProJectDatasci0812/urls.py
@@ -1,4 +1,4 @@
-"""DatascienceProJect URL Configuration
+"""ProJectDatasci0812 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DatascienceProJect.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProJectDatasci0812.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
